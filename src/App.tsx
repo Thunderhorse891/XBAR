87d9d6640906fc70514b9663ae790767ff49492f
Thunderhorse891 <erinswyrick85@gmail.com>
2025-10-23 12:18:57 -0500

    Add App.tsx - base shell of XBAR app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card } from "!@/components/ui/card";
+
+function App () {
+  return (
+    <main className="p-4">
+      <Card>
+        <h1>Welcome to XB!R</h1>
+        <p>Start building your power-user tool.</p>
+      </Card>
+    </main>
+  );
+}
+export default App;
