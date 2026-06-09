96b969bdd9d8020247566b5cb8b805d81dd6f735
Thunderhorse891 <erinswyrick85@gmail.com>
2026-06-09 14:30:32 -0500

    Add offline shell service worker

---
+++ public/xbar-service-worker.js
@@ -0,0 +1,35 @@
+const XBAR_CACHE = 'xbar-offline-shell-v1';
+const SHELL_ASSETS = ['/', '/index.html', '/manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(XBAR_CACHE).then((cache) => cache.addAll(SHELL_ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== XBAR_CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(fetch(request).catch(() => caches.match('/index.html')));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).then((response) => {
+      const clone = response.clone();
+      caches.open(XBAR_CACHE).then((cache) => cache.put(request, clone));
+      return response;
+    }).catch(() => cached)),
+  );
+});
